7b01b5a63e32057f0110830c3ca8dd996b1a091b
Deybi <Deybi@DESKTOP-M6II9IU>
2025-05-17 22:54:44 -0500

    Prueba 2

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -3,11 +3,16 @@ package Principal;
 public class Main {
 
     public static void main(String[] args) {
-            // este es el inicio de todo ssssssssssssssssssssssss          
+        // este es el inicio de todo ssssssssssssssssssssssss          
+
+        /* modulo empleados 3  ---- mpdulo gestion de reserva 
             
-/* modulo empleados 3  ---- mpdulo gestion de reserva */
-
-
+            BASES DE DATOS PLANEADAS:
+- Gestion de empleados.
+- Gestion de reservas
+- Usuarios (Inicio de sesión)
+            
+         */
     }
 
 }
