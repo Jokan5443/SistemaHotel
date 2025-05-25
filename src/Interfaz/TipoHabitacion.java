90119543d94248e754503d0838d02789b4e7747d
Deybi <Deybi@DESKTOP-M6II9IU>
2025-05-24 23:25:42 -0500

    Gestor Reserva Prueba de Subida

--- src/Interfaz/TipoHabitacion.java
+++ src/Interfaz/TipoHabitacion.java
@@ -3,7 +3,8 @@ package Interfaz;
 
 
 public interface TipoHabitacion {
-/*QUE VIVA LA COMUNIDA LGTBQ*/
+/*QUE VIVA LA COMUNIDA LGTBQ*/ 
+    /*YA SABEMOS QUE ERES GAY*/
     String precio();
     String tipoHabitacion();
     
